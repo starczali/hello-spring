f54acfa2e9995446990f5c2fe502a7c3bc363d5c
SandorTarczali <sandor@nexperta.com>
2026-04-30 11:50:03 +0300

    reformat code

--- src/main/java/com/example/hellospring/HelloController.java
+++ src/main/java/com/example/hellospring/HelloController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-  public class HelloController {
+public class HelloController {
 
     @GetMapping("/hello")
-        public ResponseEntity<String> hello() {
-                  return ResponseEntity.ok("Hello Sandor!");
-        }
-  }
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello Sandor!");
+    }
+}
