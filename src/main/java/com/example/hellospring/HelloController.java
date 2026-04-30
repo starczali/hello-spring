9cb41ea00995d5907a5e8ad2e0fc909ffdd87e2b
starczali <30625273+starczali@users.noreply.github.com>
2026-04-30 11:43:31 +0300

    Update HelloController.java

--- src/main/java/com/example/hellospring/HelloController.java
+++ src/main/java/com/example/hellospring/HelloController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
   public class HelloController {
 
     @GetMapping("/hello")
-                return ResponseEntity.ok("Sandor egy ugyes fiu");
-                  
+        public ResponseEntity<String> hello() {
+                  return ResponseEntity.ok("Hello Sandor!");
+        }
   }
